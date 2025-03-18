14d884e9ef93bfb886ab0b1a88722456adce8eb6
esraelabebe <esruabebe@gmail.com>
2025-03-17 19:24:27 -0500

    get random emojis

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,31 @@ function App() {
     try {
       const response = await getEmojisData();
       const dataSample = response.slice(0, 5);
-      setEmojisData(dataSample);
+      console.log(getRandomIndices(response));
 
+      setEmojisData(dataSample);
       setIsGameOn(true);
     } catch (err) {
       console.error(err);
     }
   }
 
+  function getRandomIndices(data) {
+    const randomIndicesArray = [];
+      for (let i = 0; i < 5; i++) {
+        const randomNum = Math.floor(Math.random() * data.length)
+        if (!randomIndicesArray.includes(randomNum)) {
+          randomIndicesArray.push(randomNum)
+        }else {
+          i--
+        } 
+      }
+      return randomIndicesArray;
+  }
+
   return (
     <main>
-      <h1>Memory</h1>
+      <h1>Memory Game</h1>
       {!isGameOn && <Form handleSubmit={startGame} />}
       {isGameOn && <MemoryCard data={emojisData}/>}
     </main>
