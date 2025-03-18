e328004070aee11456dbccafeeb6ba2cc42609c2
esraelabebe <esruabebe@gmail.com>
2025-03-18 00:38:05 -0500

    Edge cases for rendering emojis

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,10 +1,17 @@
 function MemoryCard({ handleClick, data }) {
-  const emojiEl = data.map((emojiElement, index) => (
-    <li key={index}>
-      <button onClick={handleClick}>{emojiElement.emoji}</button>
-    </li>
-  ));
-  return <ul className="card-container">{emojiEl}</ul>;
+  if (!data || data?.length === 0) {
+    return <p>Data not available at the moment. Please try again later.</p>;
+  }
+
+  return (
+    <ul className="card-container">
+      {data.map((emojiElement, index) => (
+        <li key={index}>
+          <button onClick={handleClick}>{emojiElement.emoji}</button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default MemoryCard;
