ec6eb3a1c797f74f1554ca1f08d2ec9a80fd7d9a
esraelabebe <esruabebe@gmail.com>
2025-05-25 13:25:13 -0500

    Make AssistiveTechInfo component accessible

--- src/components/AssistiveTechInfo.jsx
+++ src/components/AssistiveTechInfo.jsx
@@ -2,7 +2,7 @@ import "./AssistiveTechInfo.css";
 
 function AssistiveTechInfo({ emojisData, matchedCards }) {
   return (
-    <section className="sr-only">
+    <section className="sr-only" aria-live="polite" aria-atomic="true">
       <h2>Game Status</h2>
       <p>Number of matched cards: {matchedCards.length / 2}</p>
       <p>
