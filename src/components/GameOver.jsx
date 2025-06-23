194e91fe261da512393ee8395d0f97de3e97fb62
esraelabebe <esruabebe@gmail.com>
2025-06-23 16:40:14 -0500

    Make GameOver component accessible

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,15 +1,26 @@
+import { useRef, useEffect } from "react";
 import "./GameOver.css";
 import RegularButton from "./RegularButton";
 
-function GameOver({handleClick}) {
+function GameOver({ handleClick }) {
+  /**
+   *Add focus to this new DOM node and prompt screen readers to read its content 
+   *aloud as soon as it renders.
+   */
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    divRef.current.focus();
+  },[]);
+
   return (
-    <div className="wrapper wrapper--accent">
+    <div className="wrapper wrapper--accent" ref={divRef} tabIndex={-1}>
       <p className="p--large">You've matched all the memory cards!</p>
       {/**
        *Render an instance of the "RegularButton" component
        *This button should reset the game and return the user to the initial form displayed at the start.
        */}
-      <RegularButton handleClick={handleClick}>Play Again </RegularButton>
+      <RegularButton handleClick={handleClick}>Play Again</RegularButton>
     </div>
   );
 }
