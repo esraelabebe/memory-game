a113f5cbf5ee67f9bc153be3858c2cfeaa0ecaa0
esraelabebe <esruabebe@gmail.com>
2025-08-08 15:07:16 -0500

    fix event handler function

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,8 +1,8 @@
 import RegularButton from "./RegularButton";
 
 function Level({ formData, setFormData, startGame, resetGame }) {
-  const newFormDataNumber = formData.number + 10;
   const handleNextLevel = (e) => {
+    const newFormDataNumber = formData.number + 10;
     // When next level is selected add 10 and set formData state.
     setFormData({
       group: formData.group,
@@ -10,7 +10,6 @@ function Level({ formData, setFormData, startGame, resetGame }) {
     });
     resetGame();
     startGame(e, newFormDataNumber);
-    return newFormDataNumber;
   };
 
   return (
